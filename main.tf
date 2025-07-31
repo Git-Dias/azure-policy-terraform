df16172b13fc5262744776d958e76dc15e77ef66
DiazGIT <cloud.dias@outlook.com>
2025-07-30 23:03:23 -0300

    Ajusta Provider

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
-resource "azurerm_policy_assignment" "api_management" {
+resource "azurerm_subscription_policy_assignment" "api_management" {
   name                 = "API Management APIs should use only encrypted protocols"
   policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/ee7495e7-3ba7-40b6-bfee-c29e22cc75d4"
-  scope                = var.scope
-  display_name         = "API Management APIs should use only encrypted protocols"
-  description          = "To ensure security of data in transit, APIs should be available only through encrypted protocols, like HTTPS or WSS. Avoid using unsecured protocols, such as HTTP or WS."
+  subscription_id      = var.scope
 }
 
 
